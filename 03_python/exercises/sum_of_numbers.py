1bb910f58fcac34ed355a6b267e00adb52154fcb
Kajal Raju Kale <kajalkale245@gmail.com>
2024-07-05 23:47:42 -0700

    change text

--- 03_python/exercises/sum_of_numbers.py
+++ 03_python/exercises/sum_of_numbers.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
+
 def main():
     maxVal = 20
     sum = 0
-    
-    for i in range(0, maxVal+1):
+
+    for i in range(0, maxVal + 1):
         sum = sum + i
         print(sum, i)
-    
-    print(f"Sum: {sum}")
+
+    print(f"This is sum: {sum}")
 
 
 if __name__ == "__main__":
