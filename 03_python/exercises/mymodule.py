a71c43752a31d7c4680714eca5f20c3713ef263c
Kajal Raju Kale <kajalkale245@gmail.com>
2024-07-15 20:42:24 -0700

    push

--- 03_python/exercises/mymodule.py
+++ 03_python/exercises/mymodule.py
@@ -2,3 +2,8 @@ def greeting(name):
   print("Hello, " + name)
 
   
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
--- 03_python/exercises/working-with-modules.py
+++ 03_python/exercises/working-with-modules.py
@@ -1,3 +1,5 @@
 import mymodule
 
 mymodule.greeting("Jonathan")
+
+print(mymodule.person1)
